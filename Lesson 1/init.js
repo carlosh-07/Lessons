e7362a036430509bc65d109ee11b50a947000262
carlosh-07 <95hernandezcarlos@gmail.com>
2023-05-11 20:33:11 -0400

    Lessons
    Lessons

---
+++ Lesson 1/init.js
@@ -0,0 +1,14 @@
+function init() {
+  const ashNode = document.createElement("img");
+  ashNode.src =
+    "https://lh3.googleusercontent.com/drive-viewer/AAOQEORmwUk8swxiB7yqCk8TKDKNFphgAFCY0BY6_93KG6EHqpqY-EtRVl9PoixtaBpskZCJWzJHje5DlVL5CM9h-m5y1rVTBQ=s2560";
+  ashNode.id = "ash";
+
+  const seat = document.getElementById("seat3");
+  seat.appendChild(ashNode);
+}
+
+
+module.exports = {
+    init,
+  };
